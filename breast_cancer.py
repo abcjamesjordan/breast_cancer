3818e3d39ff5e160615a5db27a40d76eb6c48a9f
James Jordan <30762515+abcjamesjordan@users.noreply.github.com>
2020-09-25 14:46:28 -0600

    Add files via upload

--- breast_cancer.py
+++ breast_cancer.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import math
 
 # Importing the data into a pandas DataFrame
 bc = pd.read_csv('breast-cancer-wisconsin.csv', header=None, delimiter=',')
@@ -18,6 +19,84 @@ bc.columns = col_replace
 bc = bc[~bc['F'].isin(['?'])]
 bc['F'] = pd.to_numeric(bc['F'])
 
-print(bc.head())
+# Split data into train and test sets
+bc_len = len(bc.index)
+bc_train_len = int(math.floor(bc_len * .6))
+bc_test_len = int(math.ceil(bc_len * .2))
 
-bc.to_csv('bc_cleaned.csv')
+bc_train = bc.iloc[0:bc_train_len, :]
+bc_dev = bc.iloc[bc_train_len:(bc_train_len + bc_test_len), :]
+bc_test = bc.iloc[(bc_train_len + bc_test_len):, :]
+
+bc_train_x = bc_train.iloc[:, :9]
+bc_dev_x = bc_dev.iloc[:, :9]
+bc_test_x = bc_test.iloc[:, :9]
+
+bc_train_y = bc_train.iloc[:, -1]
+bc_dev_y = bc_dev.iloc[:, -1]
+bc_test_y = bc_test.iloc[:, -1]
+
+print(bc_train_x.shape, bc_train_y.shape)
+
+#print(bc_train.shape, bc_dev.shape, bc_test.shape)
+
+
+# Begin the Machine Learning Portion
+
+# Convert data to numpy arrarys
+
+bc_train_x = bc_train_x.to_numpy().T
+bc_dev_x = bc_dev_x.to_numpy().T
+bc_test_x = bc_test_x.to_numpy().T
+
+bc_train_y = bc_train_y.to_numpy().T
+bc_dev_y = bc_dev_y.to_numpy().T
+bc_test_y = bc_test_y.to_numpy().T
+
+print(bc_train_x.shape, bc_train_y.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# End
